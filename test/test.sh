ba79996fc4904a4899ea9f7418050ce1a3a4f4a8
Michael Stapelberg <michael@stapelberg.de>
2013-01-30 20:03:42 +0100

    bugfix: remove test files in test.sh with exit trap, not in Makefile.am
    otherwise, the files are removed from srcdir when running "make clean"

--- test/test.sh
+++ test/test.sh
@@ -7,9 +7,8 @@ rm -f test.txt_ test.new
 
 # We need to copy the source files because the output file will be placed in
 # the same directory. However, $srcdir might not be writable, while $PWD is.
-if [ ! -e "test.txt" ]; then
-    cp $srcdir/test.txt $srcdir/test.compressed .
-fi
+cp $srcdir/test.txt $srcdir/test.compressed .
+trap 'rm -f test.txt test.compressed' INT TERM EXIT
 
 $mscompress_abspath/src/mscompress test.txt
 $mscompress_abspath/src/msexpand < test.txt_ > test.new
