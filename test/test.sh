c28020b4a72089f613e96b969ebcb43a05595c2b
Michael Stapelberg <michael@stapelberg.de>
2013-01-30 19:50:33 +0100

    properly run the tests via autotools
    "make distcheck" now passes

--- test/test.sh
+++ test/test.sh
@@ -2,8 +2,14 @@
 
 set -e
 
+# Clean up old test output, just in case "make check" is run twice.
 rm -f test.txt_ test.new
-../src/mscompress test.txt
-../src/msexpand < test.txt_ > test.new
+
+# We need to copy the source files because the output file will be placed in
+# the same directory. However, $srcdir might not be writable, while $PWD is.
+cp $srcdir/test.txt $srcdir/test.compressed .
+
+$mscompress_abspath/src/mscompress test.txt
+$mscompress_abspath/src/msexpand < test.txt_ > test.new
 diff -q test.compressed test.txt_
 diff -q test.new test.txt
